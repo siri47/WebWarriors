1b7643125a898e904e511d77b9dee021ead9dc4f
somdeep <somdeepdey10@gmail.com>
2016-04-25 07:52:49 +0000

    Basic individual read,insert,update,delete and read all

--- php/hello-world.php
+++ php/hello-world.php
@@ -1,36 +1,53 @@
 <html><body>
 <?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
+
 require_once __DIR__ . "/vendor/autoload.php";
-echo 'Hello world from Cloud9!';
-	$m = new MongoDB\Client("mongodb://somdeep:root@ds041861.mlab.com:41861/warrior");
-    //echo extension_loaded("mongo") ? "loaded\n" : "not loaded\n";
-   ;
-    $db=$m->warrior;
+	  $m = new MongoDB\Client("mongodb://somdeep:root@ds041861.mlab.com:41861/warrior");
    
-    // $db->array('messages');
-    // $collection=$db->users;
-    // $cursor = $collection->find();
-    $collection=$db->work;
-    $cursor=$collection->findOne();
-    //echo $collection;
-    // foreach ($cursor as $id => $value) {
-    //     echo "$id:";
-    //     var_dump($value);
-    // 
-    // }
+   
+   $db=$m->warrior;
+   
+   endl(); 
+   echo "Somdeep Dey";
+   endl();
+   $collection=$db->work;
+   $cursor=$collection->find();
     
-    var_dump($cursor);
-    
-    // $document = $collection->findOne(['username'=> 'Somdeep Dey']);
-    //$document = $collection->findOne(['username' => 'Somdeep Dey']);
-    // foreach ($collection as $document) {
-    //     echo $document;
-    //      // code...
-    // }
-    //var_dump($document);
+        
+       
+       
+   //Read Values
+   foreach ($cursor as $id => $value) 
+   {
+    echo($value->name);
+    endl();
+   }
+   
+   
+
+   
+  
+   // Insert into collection
+   $collection->insertone(array('name' => 'Harry','job'=>'student'));
+   
+   //update collection
+   $newdata = array('$set' => array("job" => "software"));
+   //$c->update(array("firstname" => "Bob"), $newdata);
+   $collection->updateone(array("name"=>"Somdeep"),$newdata);
+   
+   //Read a single value
+   $cursor=$collection->findOne(["name"=>"Somdeep"]);
+   var_dump($cursor);
+   
+   //Delete single document
+   $collection->deleteone(array("name"=>"Harry"));
+   
+   function endl()
+   {
+     echo "<br>";
+   }
+   
+   
 ?>
 </body>
 </html>
