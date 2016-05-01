b0be056ba2be15977d3d920e6bdb21f92ad38ff5
somdeep <sd2988@columbia.edu>
2016-05-01 16:13:58 -0400

    Basic node code for image,video, and basic test added using benchmark

--- node/server.js
+++ node/server.js
@@ -6,26 +6,50 @@ var express = require('express'),
     session = require('express-session'),
     fs = require('fs');
     var mongoose    = require('mongoose');
-    var dbUrl       = "mongodb://flatemate:flatemate@ds033915.mongolab.com:33915/flatemateio";
+    var dbUrl       = "mongodb://somdeep:root@ds041861.mlab.com:41861/warrior";
     //var dbUrl = "mongodb://localhost:27017/flatmate1";
 
     var db          = mongoose.connect(dbUrl);
 
-const PORT  = 9000;
+const port  = 9000;
+var app = express();
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+app.get('/image', function(req, res){
+
+		res.sendfile('mkdir.png');
+
+	});
+
+app.get('/video', function(req, res){
+
+		res.sendfile('football.mp4');
+
+	});
 
 
 
-function handleRequest(request,response){
-    console.log(request.url);
-    response.end('It works!! Path hit: ' + response.url);
-}
-
-var server = http.createServer(handleRequest);
-
-server.listen(PORT,function(){
-    console.log("Server listening on : http://localhost:%s",PORT);
-
+app.listen(port, function(){
+  console.log('listening on port', port);
 });
+
+
+
+
+
+
+// function handleRequest(request,response){
+//     console.log(request.url);
+//     response.end('It works!! Path hit: ' + response.url);
+// }
+
+// var server = http.createServer(handleRequest);
+
+// server.listen(PORT,function(){
+//     console.log("Server listening on : http://localhost:%s",PORT);
+
+// });
 
 
 
